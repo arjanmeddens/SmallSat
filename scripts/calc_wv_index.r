20d8bca19b96112605c9cb204824728e297eb1da
Abhinav Shrestha <abhinavs@uidaho.edu>
2024-08-19 22:20:41 -0400

    Update calc_wv_index.r

--- scripts/calc_wv_index.r
+++ scripts/calc_wv_index.r
@@ -7,15 +7,32 @@
 # Updated to include 4 band 2021 NAIP images -- 19 May 2022
 # Updated to include 5 band MX RedEdge Sensor (UAS) -- 06 Jan 2023
 #-------------------------------------------------------------------#
-calc_wv_index = function(wv_data,num_bands=num_bands) {
-  if (class(wv_data) == "RasterBrick") {
-    #print(“wv_data not specified”)
+
+calc_wv_index = function(input_img,num_bands=num_bands) {
+    
+    #' Calculate index for WorldView2, NAIP, MX Rededge imagery
+    #'
+    #' @param input_img input imagery
+    #' @param num_bands number of bands of the `input_img`. `num_bands = 4` for NAIP imagery, `num_bands = 5` for MX RedEdge Sensor (UAS), and `num_bands = 8` for WorldView 2 imagery
+    #'
+    #' @return input_imagery with calculated indices and renamed bands.
+    #'   Bands returned (in order) for:
+    #'   * NAIP imagery input: `c('BLU','GRE','RED','NR1','NDVI1','EVI','RGI')`
+    #'   * MX RedEdge input: `c('BLU','GRE','RED', 'REDEDGE', 'NIR', 'NDVI', 'NDRE', 'SR', 'GLI', 'RGI')`
+    #'   * WorldView imagery input: `c('OCE','BLU','GRE','YEL','RED','EDG','NR1','NR2','NDVI1','NDVI2','NDRE1','NDRE2','EVI','RGI')`
+    #' @export
+    #'
+    #' @examples
+    
+    
+  if (class(input_img) == "RasterBrick") {
+    #print(“input_img not specified”)
   } else {
-    message(paste('wv_data should be a RasterBrick'))
+    message(paste('input_img should be a RasterBrick'))
   }
 
   if (length(num_bands) == 0) {
-    num_bands == dims(wv_data)[3]
+    num_bands == dims(input_img)[3]
     message(paste('num_bands taken from RasterBrick...'))
   } else {
     message(paste('num_bands is ',num_bands))
@@ -37,20 +54,20 @@ calc_wv_index = function(wv_data,num_bands=num_bands) {
      message(paste(Sys.time()))
      list_return = c('OCE','BLU','GRE','YEL','RED','EDG','NR1','NR2','NDVI1',
                      'NDVI2','NDRE1','NDRE2','EVI','RGI')
-     OCE   =  wv_data[[1]]
-     BLU   =  wv_data[[2]]
-     YEL   =  wv_data[[3]]
-     GRE   =  wv_data[[4]]
-     RED   =  wv_data[[5]]
-     EDG   =  wv_data[[6]]
-     NR1   =  wv_data[[7]]
-     NR2   =  wv_data[[8]]
-     NDVI1 = (wv_data[[7]]-wv_data[[5]])/(wv_data[[7]]+wv_data[[5]])
-     NDVI2 = (wv_data[[8]]-wv_data[[5]])/(wv_data[[8]]+wv_data[[5]])
-     NDRE1 = (wv_data[[7]]-wv_data[[6]])/(wv_data[[7]]+wv_data[[6]])
-     NDRE2 = (wv_data[[8]]-wv_data[[6]])/(wv_data[[8]]+wv_data[[6]])
-     EVI   = c_g*(wv_data[[7]]-wv_data[[5]])/(wv_data[[7]]+c_c1*wv_data[[5]]+c_c2*wv_data[[2]]+c_l)
-     RGI   = wv_data[[5]]/wv_data[[4]]
+     OCE   =  input_img[[1]]
+     BLU   =  input_img[[2]]
+     YEL   =  input_img[[3]]
+     GRE   =  input_img[[4]]
+     RED   =  input_img[[5]]
+     EDG   =  input_img[[6]]
+     NR1   =  input_img[[7]]
+     NR2   =  input_img[[8]]
+     NDVI1 = (input_img[[7]]-input_img[[5]])/(input_img[[7]]+input_img[[5]])
+     NDVI2 = (input_img[[8]]-input_img[[5]])/(input_img[[8]]+input_img[[5]])
+     NDRE1 = (input_img[[7]]-input_img[[6]])/(input_img[[7]]+input_img[[6]])
+     NDRE2 = (input_img[[8]]-input_img[[6]])/(input_img[[8]]+input_img[[6]])
+     EVI   = c_g*(input_img[[7]]-input_img[[5]])/(input_img[[7]]+c_c1*input_img[[5]]+c_c2*input_img[[2]]+c_l)
+     RGI   = input_img[[5]]/input_img[[4]]
      message(paste('Calculated:'))
      message(paste(list_return,'...'))
      message(paste('Processing time:'))
@@ -69,13 +86,13 @@ calc_wv_index = function(wv_data,num_bands=num_bands) {
      message(paste(Sys.time()))
      ptm <- Sys.time()
      list_return = c('BLU','GRE','RED','NR1','NDVI1','EVI','RGI')
-     BLU   =  wv_data[[1]]
-     GRE   =  wv_data[[2]]
-     RED   =  wv_data[[3]]
-     NR1   =  wv_data[[4]]
-     NDVI1 = (wv_data[[4]]-wv_data[[3]])/(wv_data[[4]]+wv_data[[3]])
-     EVI = c_g*(wv_data[[4]]-wv_data[[3]])/(wv_data[[4]]+c_c1*wv_data[[3]]+c_c2*wv_data[[1]]+c_l)
-     RGI   = wv_data[[3]]/wv_data[[2]]
+     BLU   =  input_img[[1]]
+     GRE   =  input_img[[2]]
+     RED   =  input_img[[3]]
+     NR1   =  input_img[[4]]
+     NDVI1 = (input_img[[4]]-input_img[[3]])/(input_img[[4]]+input_img[[3]])
+     EVI = c_g*(input_img[[4]]-input_img[[3]])/(input_img[[4]]+c_c1*input_img[[3]]+c_c2*input_img[[1]]+c_l)
+     RGI   = input_img[[3]]/input_img[[2]]
      message(paste('Calculated:'))
      message(paste(list_return,'...'))
      message(paste('Processing time:'))
@@ -86,7 +103,7 @@ calc_wv_index = function(wv_data,num_bands=num_bands) {
   #-- For 5 Band RedEdge MX (UAV MS sensor) DATA --#
   if (num_bands == 5) {
 
-    img = wv_data
+    img = input_img
     tmp_img <- (img/10000) 
     
     ptm <- Sys.time()
